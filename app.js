463629e5edc407c29d095a5b27482a83515bca50
Aashis Rimal <aashis_rimal@yahoo.com>
2018-02-10 16:29:28 +0200

    Angular data bindings and usage

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ angular.module('learndigest', []);
 
 // Declare controller, inject scope variable in controller
 angular.module('learndigest').controller('PlaygroundCtrl', function ($scope) {
-    
+    //Add new attribute on scope
+    $scope.force = 30;
+
+    //Add function to reset force to 0
+    $scope.resetForce = function () {
+        $scope.force = 0;
+    }
 });
 
