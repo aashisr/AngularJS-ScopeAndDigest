eba5274b0926f243fe6b725fccf485888eb93fac
Aashis Rimal <aashis_rimal@yahoo.com>
2018-02-10 16:50:55 +0200

    using controllerAs syntax for  prototype inheritance

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ angular.module('learndigest', []);
 
 // Declare controller, inject scope variable in controller
 angular.module('learndigest').controller('PlaygroundCtrl', function ($scope) {
+    //Create a new object vm,
+    // vm stores all other variables we need in addition to this scope
+    var vm = this;
+
+    //Bind vm to scope, playground is a variable
+    //This line can be removed and a special syntax can be added in ng-controller (PlaygroundCtrl as playground)
+    //$scope.playground = vm;
+
     //Add new attribute on scope
-    $scope.force = 30;
+    //Now, $scope can be changed to vm
+    vm.force = 30;
 
     //Add function to reset force to 0
-    $scope.resetForce = function () {
-        $scope.force = 0;
+    vm.resetForce = function () {
+        vm.force = 0;
     }
 });
 
@@ -18,6 +27,8 @@ angular.module('learndigest').controller('PlaygroundCtrl', function ($scope) {
 //Scope of the Playground controller is parent of the scope of Internal controller
 //Scope in internal controller has access to variables defined in scope of Playground controller
 //Variables in InternalCtrl can not be accessed in PlaygroundCtrl
+
+//Now, scope in this InternalCtrl have access only to the playground variable
 angular.module('learndigest').controller('InternalCtrl', function ($scope) {
-    $scope.secret = "shhhh" + $scope.force;
+    $scope.secret = "shhhh" + $scope.playground.force;
 });
