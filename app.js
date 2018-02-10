efd8baabd9b996d7e2d36e4efd2caaedb122e02e
Aashis Rimal <aashis_rimal@yahoo.com>
2018-02-11 00:04:26 +0200

    deep , watching changes in properties' of object

--- app.js
+++ app.js
@@ -5,31 +5,33 @@ angular.module('learndigest', []);
 angular.module('learndigest').controller('PlaygroundCtrl', function ($scope) {
     var vm = this;
 
+    //Create new object userProperties to watch this object
+    vm.userProperties = {
+        favoriteColor: 'white',
+        nickname: 'aashis',
+        force: 30
+    };
+
     //Adding new variable to use ng-if statement
     vm.enableForceEdit = true;
 
     //Add new attribute on scope
-    vm.force = 30;
+    //Update force to use userProperties force
+    vm.userProperties.force = 30;
 
     //Add function to reset force to 0
+    //Now, change whole of user properties object so as to deep watch
     vm.resetForce = function () {
-        vm.force = 0;
+        vm.userProperties.force = 0;
     }
 
-    //Add a watch statement
-    //First argument is a string which represents an expression expression we want to watch
-    //Second argument will be a function that will run whenever the value of first argument changes
-    $scope.$watch('vm.force', function (newValue, oldValue) {
-        console.log('Force value has changed', vm.force, newValue, oldValue);
-    })
 
-    //Function that returns a value can be watched as well
-    //Function to watch if the force is even or odd
-    //Prints in console only when value has changed or in this case when even changes to odd  or vice-versa
-    $scope.$watch(function() { return vm.force % 2 === 0; }, function (newValue, oldValue) {
-        //If force is even, return even, if not return odd
-        console.log(newValue ? 'force is even' : 'force is odd');
-    })
+    //Deep watch whole vm.userProperties object
+    //Third argument 'true'  tells angular that this watch is a deep watch
+    //That means if any property on the vm.userProperties is changed, it prints the changed value in console
+    $scope.$watch('vm.userProperties', function () {
+        console.log('User properties changed:', vm.userProperties)
+    }, true);
 });
 
 
