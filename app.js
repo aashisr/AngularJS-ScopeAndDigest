f9020188f590d9f081fae56b950e78dcafb8d2d0
Aashis Rimal <aashis_rimal@yahoo.com>
2018-02-10 16:19:26 +0200

    Initializing an app and a controller

---
+++ app.js
@@ -0,0 +1,8 @@
+//Declared app named learndigest
+angular.module('learndigest', []);
+
+// Declare controller, inject scope variable in controller
+angular.module('learndigest').controller('PlaygroundCtrl', function ($scope) {
+    
+});
+
